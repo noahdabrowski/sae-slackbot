04563ddafdb096c6f55af59cd95fb083642f530b
noahdabrowski <noah.dabrowski@michiganwildlife.com>
2017-04-07 00:07:27 -0400

    sae-slackbot: Commit 4. Tested and fixed up the .py file, most commands work now! Testing out some ssh shit to see if i can host it and get it to run remotely, and edit shit and run it from work

--- sae-slackbot.py
+++ sae-slackbot.py
@@ -1,5 +1,6 @@
 import os
 import time
+import sys
 from slackclient import SlackClient
 
 
@@ -11,6 +12,9 @@ AT_BOT = "<@" + BOT_ID + ">"
 
 #help command
 HELP_COMMAND = "help"
+
+#kill command to use in testing
+KILL_COMMAND = "kill"
 
 #parrot command, might eventually be able to message the bot in a private message telling it what channel to say it in?
 PARROT_COMMAND = "say"
@@ -28,88 +32,90 @@ slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 
 def handle_command(command, channel):
-    """
-        Receives commands directed at the bot and determines if they
-        are valid commands. If so, then acts on the commands. If not,
-        returns back what it needs for clarification.
-    """
-    response = "Wat? I don't get it. Use the *" + HELP_COMMAND + \
+	"""
+        	Receives commands directed at the bot and determines if they
+        	are valid commands. If so, then acts on the commands. If not,
+        	returns back what it needs for clarification.
+	"""
+	response = "Wat? I don't get it. Use the *" + HELP_COMMAND + \
                "* command to figure out what the fuck you're tryna say."
 
-    if command.startswith(HELP_COMMAND):#if help
+	if command.startswith(HELP_COMMAND):
 		command = command[5:] #substring the command for easier use in finding out what to do subsequently
-        response = "Heres a list of commands: help, say, eve.\nuse *help xxxx* to learn more about a command, where xxxx is the command name."
-		if command.startswith(EVE_COMMAND)#if theyre asking for help with eve specifically
-			response = "list of subcommands for the eve command:\ndied: use *eve died xxxx* to record a player death, where xxxx is the player name.\n" +
-			"killed: use *eve killed xxxx* to record a group kill, where xxxx is the name of the ship that the group killed.\n" +
-			"deaths: use *eve deaths* to display the total group death count, or *eve deaths xxxx* to display a specific players death count.\n" +
-			"kills: use *eve kills* to display group kill list.\n" + 
-			"adduser: use *eve adduser xxxx* to add a new eve user to be able to track deaths and kills, where xxxx is user name."
-	
-    if command.startswith(PARROT_COMMAND):#if say
-		response = "Haven't written this command yet lul. git rekt"
-	
-	if command.startswith(EVE_COMMAND):#if eve
-		command = command[4:] #substring the command for easier use in finding out what to do subsequently
-		response = "FRIGATE FLEET TUESDAYS BOIIIIIII. If you actually wanna do something with this command then try *help eve*."
-		if command.startswith(EVE_DIED_COMMAND)
-			command = command[5:]#further substring the command for use
+        	response = "Heres a list of commands: help, say, eve.\nuse *help xxxx* to learn more about a command, where xxxx is the command name."
+		if command.startswith(EVE_COMMAND):#if theyre asking for help with eve specifically
+			evehelp1 = "list of subcommands for the eve command:\ndied: use *eve died xxxx* to record a player death, where xxxx is the player name.\n"
+			evehelp2 = "killed: use *eve killed xxxx* to record a group kill, where xxxx is the name of the ship that the group killed.\n"
+			evehelp3 = "deaths: use *eve deaths* to display the total group death count, or *eve deaths xxxx* to display a specific players death count.\n"
+			evehelp4 = "kills: use *eve kills* to display group kill list.\n"
+			evehelp5 = "adduser: use *eve adduser xxxx* to add a new eve user to be able to track deaths and kills, where xxxx is user name."
 			
-			#for line in blah: if command.startswith() see below
-			#write to file
+			response = "%s%s%s%s%s"%(evehelp1,evehelp2,evehelp3,evehelp4,evehelp5)
+	if command.startswith(KILL_COMMAND):
+		response = "Bot will commit seppuku"
+
+	if command.startswith(PARROT_COMMAND):
+		response = "Haven't written this command yet lul. git rekt"
+
+	if command.startswith(EVE_COMMAND):
+		command = command[4:]
+		response = "FRIGATE FLEET TUESDAYS BOIIIIIII. If you actually wanna do something with this command then try *help eve*."
+		if command.startswith(EVE_DIED_COMMAND):
+			command = command[5:]
+
 			response = "Damn boi yall suck. The selected player's death count  has been updated."
-		if command.startswith(EVE_KILLED_COMMAND)
-			command = command[7:]#further substring the command for use
+		if command.startswith(EVE_KILLED_COMMAND):
+			command = command[7:]
 			if command:
-				with open("eve_group_kills.txt", "a") as group_kills_file:
-					group_kills_file.write(command)
+				with open("sae-slackbot/eve_group_kills.txt", "a") as group_kills_file:
+					group_kills_file.write(command + "\n")
 					
 				response = "Fuckin GF dude, kill has been logged."
-			else
+			else:
 				response = "You obviously didnt kill anything if you cant even remember their name u fukin dingus."
-		if command.startswith(EVE_DEATHS_COMMAND)
-			command = command[7:]#further substring the command for use
+		if command.startswith(EVE_DEATHS_COMMAND):
+			command = command[7:]
 			
 			if command:
-				with open("eve_players.txt", "r") as eve_players:
+				with open("sae-slackbot/eve_players.txt", "r") as eve_players:
 					for line in eve_players:
-						if (line == command)
-							with open("" + command + "deaths.txt", "r") as user_death_file:
+						if (line == command):
+							with open("sae-slackbot/" + command + "deaths.txt", "r") as user_death_file:
 								deathcount = user_death_file.read()
 							
 							response = command + "has died " + deathcount + "times with the group."
-						else
+						else:
 							response = "that player don't exist homie."
-			else
-				with open("eve_group_deaths.txt", "r") as group_deaths_file:
+			else:
+				with open("sae-slackbot/eve_group_deaths.txt", "r") as group_deaths_file:
 					deathcount = group_deaths_file.read()
 				
 				response = "Total number of deaths by all group members: " + deathcount
-		if command.startswith(EVE_KILLS_COMMAND)
-			command = command[7:]#further substring the command for use. This one is probably unneccessary because there is no subsequent task
+		if command.startswith(EVE_KILLS_COMMAND):
+			command = command[7:]
 			
 			#open the file, read all of the data into the kills var, close the file
-			with open("eve_group_kills.txt", "r") as group_kills_file:
-				kills = group_kills_file.readlines()
+			with open("sae-slackbot/eve_group_kills.txt", "r") as group_kills_file:
+				killss = group_kills_file.readlines()
+				kills = "".join(killss)
 			
 			response = "Group kills:\n" + kills
-		if command.startswith(EVE_ADDUSER_COMMAND)
-			command = command[8:]#further substring the command for use.
+		if command.startswith(EVE_ADDUSER_COMMAND):
+			command = command[8:]
 			
 			if command:
-				with open("eve_players.txt", "a") as eve_players:
-					eve_players.write(command)
+				with open("sae-slackbot/eve_players.txt", "a") as eve_players:
+					eve_players.write(command + "\n")
 			
-				with open("" + command + "deaths.txt", "w") as newuser_death_file:
+				with open("sae-slackbot/" + command + "deaths.txt", "w") as newuser_death_file:
 					newuser_death_file.write("0")
 				
 				response = "User has been added and files have been created for them!"
-			else
+			else:
 				response = "please specify a user name if youre gonna use this command fam."
 			#add user
 
-    slack_client.api_call("chat.postMessage", channel=channel,
-                          text=response, as_user=True)
+	slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
 ###############################################################################################################################
 #todo:
@@ -148,9 +154,11 @@ if __name__ == "__main__":
     if slack_client.rtm_connect():
         print("sae-slackbot connected and running!")
         while True:
-            command, channel = parse_slack_output(slack_client.rtm_read())
-            if command and channel:
-                handle_command(command, channel)
-            time.sleep(READ_WEBSOCKET_DELAY)
+		command, channel = parse_slack_output(slack_client.rtm_read())
+		if command and channel:
+                	handle_command(command, channel)
+			if command.startswith(KILL_COMMAND):
+				sys.exit()
+			time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
